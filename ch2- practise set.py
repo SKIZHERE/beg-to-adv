1484dac3c1c7464f6fa37b8b4badfd125d548257
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-20 23:14:19 +0530

    set 2 done

---
+++ ch2- practise set.py
@@ -0,0 +1,45 @@
+""""""
+# 1
+"""
+a = 1
+b = 2
+c = a+b
+print("The sum of numbers is ",c)
+"""
+
+# 2
+"""
+a = 10
+b = 3
+print("The reminder when",a,"is divided by",b,"is",a%b)
+"""
+
+# 3
+"""
+var = eval(input("enter something of a datatype\n"))
+tyy = type(var)
+print(tyy)
+"""
+
+# 4
+"""
+a = 83
+b = 80
+if (a>b): print(a," is greater than ",b)
+else: print(b," is greater than ",a)
+"""
+
+# 5
+"""
+a = int(input("number 1:"))
+b = int(input("number 2:"))
+c = (a+b)/2
+print("The average of given two numbers is", c)
+"""
+
+# 6
+"""
+num = float(input("Number to be squared:"))
+sq = num**2
+print("The square of the given number is",sq)
+"""
