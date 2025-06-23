1956b3633bd98f45ca8a817d90ae7a7709fdbb97
SKIZHERE <kumarsaksham67@gmail.com>
2025-06-23 20:45:38 +0530

    set 4 done

---
+++ ch4- practise set.py
@@ -0,0 +1,52 @@
+""""""
+# 1
+"""
+l=[]
+for i in range(1,8):
+    a = str(input(f"Fruit no {i}:"))
+    l.append(a)
+print(l)
+#"""
+
+# 2
+"""
+l=[]
+for i in range(1,7):
+    a = int(input("Marks : "))
+    l.append(a)
+l.sort()
+print(l)
+"""
+
+# 3
+"""
+s = (1,6,7)
+s[2] = "hi"
+"""
+
+# 4
+"""
+l = [1,5,7,8]
+print(sum(l))
+"""
+
+# 5
+"""
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
